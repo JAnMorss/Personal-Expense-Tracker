5c83226d81b1e3aa388ac9eb0fb413d164daa3e9
JAnMorss <johnanthonymorales124@gmail.com>
2025-12-01 21:56:18 +0800

    feat(infrastructure): add ApplicationDbContext and infrastructure setup

---
+++ src/SpendWise.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpendWise.Domain.Users.Entities;
+
+namespace SpendWise.Infrastructure;
+
+public sealed class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+        : base(options)
+    {
+        
+    }
+
+    public DbSet<User> Users { get; set; }
+}
