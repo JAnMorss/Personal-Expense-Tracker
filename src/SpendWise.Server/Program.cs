4f925c48ee9e33f0610ee4d64bba771ddce0ef0f
JAnMorss <johnanthonymorales124@gmail.com>
2025-12-05 20:34:36 +0800

    feat(api): configure server startup, middleware, and API extensions

--- src/SpendWise.Server/Program.cs
+++ src/SpendWise.Server/Program.cs
@@ -1,20 +1,38 @@
+using Serilog;
+using SpendWise.Infrastructure;
+using SpendWise.Application;
+using SpendWise.Server.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    app.ApplyMigrations();
 }
 
 app.UseHttpsRedirection();
+
+app.UseRequestContextLogging();
+
+app.UseSerilogRequestLogging();
+
+app.UseCustomExceptionHandler();
 
 app.UseAuthorization();
 
