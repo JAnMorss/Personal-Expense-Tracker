2fa807afb159889dc2d5caa34b9bb805cc150f08
JAnMorss <johnanthonymorales124@gmail.com>
2025-12-21 20:30:26 +0800

    Server: Add user controllers

---
+++ src/SpendWise.Server/Controllers/Users/UserController.cs
@@ -0,0 +1,58 @@
+﻿using Asp.Versioning;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SpendWise.Application.Users.Queries.GetMyProfile;
+using SpendWise.Application.Users.Queries.GetUserAvatar;
+using SpendWise.Application.Users.Response;
+using SpendWise.Server.Abstractions;
+
+namespace SpendWise.Server.Controllers.Users;
+
+[ApiController]
+[ApiVersion(ApiVersions.V1)]
+[Route("api/v{version:apiVersion}/user")]
+[Authorize]
+public class UserController : ApiController
+{
+    public UserController(ISender sender) 
+        : base(sender)
+    {
+    }
+
+    [HttpGet("me")]
+    public async Task<IActionResult> GetMyProfile(CancellationToken cancellationToken)
+    {
+        var userId = GetUserId();
+        if (userId is null)
+            return Unauthorized();
+
+        var query = new GetMyProfileQuery(userId.Value);
+
+        var result = await _sender.Send(query, cancellationToken);
+
+        return result.IsSuccess
+            ? Ok(new ApiResponse<UserResponse>(
+                result.Value,
+                "User profile retrieved successfully."))
+            : HandleFailure(result);
+    }
+
+    [HttpGet("avatar")]
+    public async Task<IActionResult> GetUserAvatar(CancellationToken cancellationToken)
+    {
+        var userId = GetUserId();
+        if (userId is null)
+            return Unauthorized();
+
+        var query = new GetUserAvatarQuery(userId.Value);
+
+        var result = await _sender.Send(query, cancellationToken);
+
+        return result.IsSuccess
+            ? Ok(new ApiResponse<UserAvatarResponse>(
+                result.Value,
+                "User avatar retrieved successfully."))
+            : HandleFailure(result);
+    }
+}
