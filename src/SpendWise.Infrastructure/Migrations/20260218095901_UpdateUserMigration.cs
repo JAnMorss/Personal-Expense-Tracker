01e06ff74466cc6e716b096612698c09621cafec
JAnMorss <johnanthonymorales124@gmail.com>
2026-02-18 18:10:31 +0800

    Add UpdateUserMigration EF Core migration

---
+++ src/SpendWise.Infrastructure/Migrations/20260218095901_UpdateUserMigration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpendWise.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_IdentityId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IdentityId",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IdentityId",
+                table: "Users",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_IdentityId",
+                table: "Users",
+                column: "IdentityId",
+                unique: true);
+        }
+    }
+}
